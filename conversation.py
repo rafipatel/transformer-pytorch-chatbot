e6bdb9c43813c1bbc94e73ae6b548b5374d6d92b
Rafi Patel <rafipatel3562@gmail.com>
2024-05-05 16:02:37 +0100

    Fixes & working files

---
+++ conversation.py
@@ -0,0 +1,74 @@
+import json
+import torch
+from torch.utils.data import Dataset
+import torch.utils.data
+from models import *
+from utils import *
+from preprocess import *
+from train import *
+
+load_checkpoint = True
+ckpt_path = r"C:\Users\rafip\Downloads\checkpoint_256_250.pth.tar"
+ckpt_path_1 = r"C:\Users\rafip\Downloads\checkpoint_256_100.pth.tar"
+
+
+
+def evaluate(transformer, question, question_mask, max_len, word_map):
+    """
+    Performs Greedy Decoding with a batch size of 1
+    """
+    rev_word_map = {v: k for k, v in word_map.items()}
+    transformer.eval()
+    start_token = word_map['<start>']
+    encoded = transformer.encode(question, question_mask)
+    words = torch.LongTensor([[start_token]]).to(device)
+    
+    for step in range(max_len - 1):
+        size = words.shape[1]
+        target_mask = torch.triu(torch.ones(size, size)).transpose(0, 1).type(dtype=torch.uint8)
+        target_mask = target_mask.to(device).unsqueeze(0).unsqueeze(0)
+        decoded = transformer.decode(words, target_mask, encoded, question_mask)
+        predictions = transformer.logit(decoded[:, -1])
+        _, next_word = torch.max(predictions, dim = 1)
+        next_word = next_word.item()
+        if next_word == word_map['<end>']:
+            break
+        words = torch.cat([words, torch.LongTensor([[next_word]]).to(device)], dim = 1)   # (1,step+2)
+        
+    # Construct Sentence
+    if words.dim() == 2:
+        words = words.squeeze(0)
+        words = words.tolist()
+        
+    sen_idx = [w for w in words if w not in {word_map['<start>']}]
+    sentence = ' '.join([rev_word_map[sen_idx[k]] for k in range(len(sen_idx))])
+    
+    return sentence
+
+
+if load_checkpoint:
+    checkpoint = torch.load(ckpt_path, map_location=torch.device('cpu'))
+    transformer = checkpoint['transformer']
+
+    checkpoint1 = torch.load(ckpt_path_1, map_location=torch.device('cpu'))
+    transformer_1 = checkpoint1['transformer']
+
+
+
+while(1):
+    question = input("Question: ") 
+    question = question.lower()
+    if question == 'quit':
+        break
+    max_len = 25 #input("Maximum Reply Length: ")
+    enc_qus = [word_map.get(word, word_map['<unk>']) for word in question.split()]
+    question = torch.LongTensor(enc_qus).to(device).unsqueeze(0)
+    question_mask = (question!=0).to(device).unsqueeze(1).unsqueeze(1)  
+    sentence = evaluate(transformer, question, question_mask, int(max_len), word_map)
+    print("250:",sentence)
+
+    # enc_qus = [word_map.get(word, word_map['<unk>']) for word in question.split()]
+    # question = torch.LongTensor(enc_qus).to(device).unsqueeze(0)
+    # question_mask = (question!=0).to(device).unsqueeze(1).unsqueeze(1)  
+    sentence1 = evaluate(transformer_1, question, question_mask, int(max_len), word_map)
+    print("100:",sentence1)
--- train.py
+++ train.py
@@ -1,1 +1,104 @@
 
+import json
+import torch
+from torch.utils.data import Dataset
+import torch.utils.data
+from models import *
+from utils import *
+
+from logger import Logger
+
+d_model = 128
+heads = 8
+num_layers = 3
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+epochs = 100
+
+with open('WORDMAP_corpus.json', 'r') as j:
+    word_map = json.load(j)
+    
+transformer = Transformer(d_model = d_model, heads = heads, num_layers = num_layers, word_map = word_map)
+transformer = transformer.to(device)
+adam_optimizer = torch.optim.Adam(transformer.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9)
+transformer_optimizer = AdamWarmup(model_size = d_model, warmup_steps = 4000, optimizer = adam_optimizer)
+criterion = LossWithLS(len(word_map), 0.1)
+
+
+
+def train(train_loader, transformer, criterion, epoch,logger):
+    
+    transformer.train()
+    sum_loss = 0
+    count = 0
+
+    for i, (question, reply) in enumerate(train_loader):
+        
+        samples = question.shape[0]
+
+        # Move to device
+        question = question.to(device)
+        reply = reply.to(device)
+
+        # Prepare Target Data
+        reply_input = reply[:, :-1]
+        reply_target = reply[:, 1:]
+
+        # Create mask and add dimensions
+        question_mask, reply_input_mask, reply_target_mask = create_masks(question, reply_input, reply_target)
+
+        # Get the transformer outputs
+        out = transformer(question, question_mask, reply_input, reply_input_mask)
+
+        # Compute the loss
+        loss = criterion(out, reply_target, reply_target_mask)
+        
+        # Backprop
+        transformer_optimizer.optimizer.zero_grad()
+        loss.backward()
+        transformer_optimizer.step()
+        
+        sum_loss += loss.item() * samples
+        count += samples
+        
+        if i % 100 == 0:
+            print("Epoch [{}][{}/{}]\tLoss: {:.3f}".format(epoch, i, len(train_loader), sum_loss/count))
+        # logger.log("Epoch [{}][{}/{}]\tLoss: {:.3f}".format(epoch, i, len(train_loader), sum_loss/count))
+            result = sum_loss/count
+            logger.log({"loss": result})
+            
+
+        
+            
+            
+def training(train_loader, transformer, criterion, epoch,logger):
+    for epoch in range(epoch):
+        
+        train(train_loader, transformer, criterion, epoch,logger)
+        
+        if epoch > 0 and (epoch % 100) == 0:
+            state = {'epoch': epoch, 'transformer': transformer, 'transformer_optimizer': transformer_optimizer}
+            print("Saving the model")
+            torch.save(state, 'checkpoint_' + str(epoch) + '.pth.tar')
+
+
+def main():
+    # Set random seed for reproducibility
+
+    train_loader = torch.utils.data.DataLoader(Dataset(),
+                                           batch_size = 100, 
+                                           shuffle=True, 
+                                           pin_memory=True)
+
+
+
+        # Initialise "wandb" for logging
+    wandb_logger = Logger(f"inm706_Transformer", project = "inm706_Trans")
+    logger = wandb_logger.get_logger()
+
+    training(train_loader, transformer, criterion, epochs,logger)
+
+
+
+if __name__ == "__main__":
+    main()
+
