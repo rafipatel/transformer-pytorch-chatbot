ea338bc6bdb22e0166831b6c180d71143c3c0e03
Rafi Patel <rafipatel3562@gmail.com>
2024-05-06 15:23:30 +0100

    minor

--- logger.py
+++ logger.py
@@ -1,11 +1,10 @@
-
 import wandb
 
 class Logger:
 
-    def __init__(self, experiment_name, logger_name = "logger", project = "inm705-cwk"):
+    def __init__(self, experiment_name, logger_name = "logger", project = "inm706-cwk"):
         logger_name = f'{logger_name}-{experiment_name}'
-        logger = wandb.init(project=project, name = logger_name)
+        logger = wandb.init(project = project, name = logger_name)
         self.logger = logger
         return
 
