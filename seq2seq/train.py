600b7c514043acc0cb0e4031ad4446839f3263fc
Rafi Patel <rafipatel3562@gmail.com>
2024-05-12 21:27:03 +0100

    bug

--- seq2seq/train.py
+++ seq2seq/train.py
@@ -320,9 +320,6 @@ def main():
            embedding, encoder_n_layers, decoder_n_layers, save_dir, n_iteration, batch_size,
            print_every, save_every, clip, corpus_name, loadFilename,logger)
            
-
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
