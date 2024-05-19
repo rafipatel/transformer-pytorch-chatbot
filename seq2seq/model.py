17e18ceaf1c23b0393c10161115e38ac69075d87
Rafi Patel <rafipatel3562@gmail.com>
2024-05-19 16:08:13 +0100

    FIXES

--- seq2seq/model.py
+++ seq2seq/model.py
@@ -16,107 +16,6 @@ import json
 from utils import *
 
 
-
-
-######################################################################
-# Define Models
-# -------------
-#
-# Seq2Seq Model
-# ~~~~~~~~~~~~~
-#
-# The brains of our chatbot is a sequence-to-sequence (seq2seq) model. The
-# goal of a seq2seq model is to take a variable-length sequence as an
-# input, and return a variable-length sequence as an output using a
-# fixed-sized model.
-#
-# `Sutskever et al. <https://arxiv.org/abs/1409.3215>`__ discovered that
-# by using two separate recurrent neural nets together, we can accomplish
-# this task. One RNN acts as an **encoder**, which encodes a variable
-# length input sequence to a fixed-length context vector. In theory, this
-# context vector (the final hidden layer of the RNN) will contain semantic
-# information about the query sentence that is input to the bot. The
-# second RNN is a **decoder**, which takes an input word and the context
-# vector, and returns a guess for the next word in the sequence and a
-# hidden state to use in the next iteration.
-#
-# .. figure:: /_static/img/chatbot/seq2seq_ts.png
-#    :align: center
-#    :alt: model
-#
-# Image source:
-# https://jeddy92.github.io/JEddy92.github.io/ts_seq2seq_intro/
-#
-
-
-######################################################################
-# Encoder
-# ~~~~~~~
-#
-# The encoder RNN iterates through the input sentence one token
-# (e.g. word) at a time, at each time step outputting an “output” vector
-# and a “hidden state” vector. The hidden state vector is then passed to
-# the next time step, while the output vector is recorded. The encoder
-# transforms the context it saw at each point in the sequence into a set
-# of points in a high-dimensional space, which the decoder will use to
-# generate a meaningful output for the given task.
-#
-# At the heart of our encoder is a multi-layered Gated Recurrent Unit,
-# invented by `Cho et al. <https://arxiv.org/pdf/1406.1078v3.pdf>`__ in
-# 2014. We will use a bidirectional variant of the GRU, meaning that there
-# are essentially two independent RNNs: one that is fed the input sequence
-# in normal sequential order, and one that is fed the input sequence in
-# reverse order. The outputs of each network are summed at each time step.
-# Using a bidirectional GRU will give us the advantage of encoding both
-# past and future contexts.
-#
-# Bidirectional RNN:
-#
-# .. figure:: /_static/img/chatbot/RNN-bidirectional.png
-#    :width: 70%
-#    :align: center
-#    :alt: rnn_bidir
-#
-# Image source: https://colah.github.io/posts/2015-09-NN-Types-FP/
-#
-# Note that an ``embedding`` layer is used to encode our word indices in
-# an arbitrarily sized feature space. For our models, this layer will map
-# each word to a feature space of size *hidden_size*. When trained, these
-# values should encode semantic similarity between similar meaning words.
-#
-# Finally, if passing a padded batch of sequences to an RNN module, we
-# must pack and unpack padding around the RNN pass using
-# ``nn.utils.rnn.pack_padded_sequence`` and
-# ``nn.utils.rnn.pad_packed_sequence`` respectively.
-#
-# **Computation Graph:**
-#
-#    1) Convert word indexes to embeddings.
-#    2) Pack padded batch of sequences for RNN module.
-#    3) Forward pass through GRU.
-#    4) Unpack padding.
-#    5) Sum bidirectional GRU outputs.
-#    6) Return output and final hidden state.
-#
-# **Inputs:**
-#
-# -  ``input_seq``: batch of input sentences; shape=\ *(max_length,
-#    batch_size)*
-# -  ``input_lengths``: list of sentence lengths corresponding to each
-#    sentence in the batch; shape=\ *(batch_size)*
-# -  ``hidden``: hidden state; shape=\ *(n_layers x num_directions,
-#    batch_size, hidden_size)*
-#
-# **Outputs:**
-#
-# -  ``outputs``: output features from the last hidden layer of the GRU
-#    (sum of bidirectional outputs); shape=\ *(max_length, batch_size,
-#    hidden_size)*
-# -  ``hidden``: updated hidden state from GRU; shape=\ *(n_layers x
-#    num_directions, batch_size, hidden_size)*
-#
-#
-
 class EncoderRNN(nn.Module):
     def __init__(self, hidden_size, embedding, n_layers=1, dropout=0):
         super(EncoderRNN, self).__init__()
@@ -144,69 +43,6 @@ class EncoderRNN(nn.Module):
         return outputs, hidden
 
 
-######################################################################
-# Decoder
-# ~~~~~~~
-#
-# The decoder RNN generates the response sentence in a token-by-token
-# fashion. It uses the encoder’s context vectors, and internal hidden
-# states to generate the next word in the sequence. It continues
-# generating words until it outputs an *EOS_token*, representing the end
-# of the sentence. A common problem with a vanilla seq2seq decoder is that
-# if we rely solely on the context vector to encode the entire input
-# sequence’s meaning, it is likely that we will have information loss.
-# This is especially the case when dealing with long input sequences,
-# greatly limiting the capability of our decoder.
-#
-# To combat this, `Bahdanau et al. <https://arxiv.org/abs/1409.0473>`__
-# created an “attention mechanism” that allows the decoder to pay
-# attention to certain parts of the input sequence, rather than using the
-# entire fixed context at every step.
-#
-# At a high level, attention is calculated using the decoder’s current
-# hidden state and the encoder’s outputs. The output attention weights
-# have the same shape as the input sequence, allowing us to multiply them
-# by the encoder outputs, giving us a weighted sum which indicates the
-# parts of encoder output to pay attention to. `Sean
-# Robertson’s <https://github.com/spro>`__ figure describes this very
-# well:
-#
-# .. figure:: /_static/img/chatbot/attn2.png
-#    :align: center
-#    :alt: attn2
-#
-# `Luong et al. <https://arxiv.org/abs/1508.04025>`__ improved upon
-# Bahdanau et al.’s groundwork by creating “Global attention”. The key
-# difference is that with “Global attention”, we consider all of the
-# encoder’s hidden states, as opposed to Bahdanau et al.’s “Local
-# attention”, which only considers the encoder’s hidden state from the
-# current time step. Another difference is that with “Global attention”,
-# we calculate attention weights, or energies, using the hidden state of
-# the decoder from the current time step only. Bahdanau et al.’s attention
-# calculation requires knowledge of the decoder’s state from the previous
-# time step. Also, Luong et al. provides various methods to calculate the
-# attention energies between the encoder output and decoder output which
-# are called “score functions”:
-#
-# .. figure:: /_static/img/chatbot/scores.png
-#    :width: 60%
-#    :align: center
-#    :alt: scores
-#
-# where :math:`h_t` = current target decoder state and :math:`\bar{h}_s` =
-# all encoder states.
-#
-# Overall, the Global attention mechanism can be summarized by the
-# following figure. Note that we will implement the “Attention Layer” as a
-# separate ``nn.Module`` called ``Attn``. The output of this module is a
-# softmax normalized weights tensor of shape *(batch_size, 1,
-# max_length)*.
-#
-# .. figure:: /_static/img/chatbot/global_attn.png
-#    :align: center
-#    :width: 60%
-#    :alt: global_attn
-#
 
 # Luong attention layer
 class Attn(nn.Module):
@@ -249,39 +85,6 @@ class Attn(nn.Module):
         return F.softmax(attn_energies, dim=1).unsqueeze(1)
 
 
-######################################################################
-# Now that we have defined our attention submodule, we can implement the
-# actual decoder model. For the decoder, we will manually feed our batch
-# one time step at a time. This means that our embedded word tensor and
-# GRU output will both have shape *(1, batch_size, hidden_size)*.
-#
-# **Computation Graph:**
-#
-#    1) Get embedding of current input word.
-#    2) Forward through unidirectional GRU.
-#    3) Calculate attention weights from the current GRU output from (2).
-#    4) Multiply attention weights to encoder outputs to get new "weighted sum" context vector.
-#    5) Concatenate weighted context vector and GRU output using Luong eq. 5.
-#    6) Predict next word using Luong eq. 6 (without softmax).
-#    7) Return output and final hidden state.
-#
-# **Inputs:**
-#
-# -  ``input_step``: one time step (one word) of input sequence batch;
-#    shape=\ *(1, batch_size)*
-# -  ``last_hidden``: final hidden layer of GRU; shape=\ *(n_layers x
-#    num_directions, batch_size, hidden_size)*
-# -  ``encoder_outputs``: encoder model’s output; shape=\ *(max_length,
-#    batch_size, hidden_size)*
-#
-# **Outputs:**
-#
-# -  ``output``: softmax normalized tensor giving probabilities of each
-#    word being the correct next word in the decoded sequence;
-#    shape=\ *(batch_size, voc.num_words)*
-# -  ``hidden``: final hidden state of GRU; shape=\ *(n_layers x
-#    num_directions, batch_size, hidden_size)*
-#
 
 class DecoderLuongAttn(nn.Module):
     def __init__(self, attn_model, embedding, hidden_size, output_size, n_layers=1, dropout=0.1,attention = bool):
@@ -336,22 +139,6 @@ class DecoderLuongAttn(nn.Module):
         return output, hidden
 
 
-######################################################################
-# Define Training Procedure
-# -------------------------
-#
-# Masked loss
-# ~~~~~~~~~~~
-#
-# Since we are dealing with batches of padded sequences, we cannot simply
-# consider all elements of the tensor when calculating loss. We define
-# ``maskNLLLoss`` to calculate our loss based on our decoder’s output
-# tensor, the target tensor, and a binary mask tensor describing the
-# padding of the target tensor. This loss function calculates the average
-# negative log likelihood of the elements that correspond to a *1* in the
-# mask tensor.
-#
-
 def maskNLLLoss(inp, target, mask):
     nTotal = mask.sum()
     crossEntropy = -torch.log(torch.gather(inp, 1, target.view(-1, 1)).squeeze(1))
@@ -360,65 +147,6 @@ def maskNLLLoss(inp, target, mask):
     return loss, nTotal.item()
 
 
-######################################################################
-# Single training iteration
-# ~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# The ``train`` function contains the algorithm for a single training
-# iteration (a single batch of inputs).
-#
-# We will use a couple of clever tricks to aid in convergence:
-#
-# -  The first trick is using **teacher forcing**. This means that at some
-#    probability, set by ``teacher_forcing_ratio``, we use the current
-#    target word as the decoder’s next input rather than using the
-#    decoder’s current guess. This technique acts as training wheels for
-#    the decoder, aiding in more efficient training. However, teacher
-#    forcing can lead to model instability during inference, as the
-#    decoder may not have a sufficient chance to truly craft its own
-#    output sequences during training. Thus, we must be mindful of how we
-#    are setting the ``teacher_forcing_ratio``, and not be fooled by fast
-#    convergence.
-#
-# -  The second trick that we implement is **gradient clipping**. This is
-#    a commonly used technique for countering the “exploding gradient”
-#    problem. In essence, by clipping or thresholding gradients to a
-#    maximum value, we prevent the gradients from growing exponentially
-#    and either overflow (NaN), or overshoot steep cliffs in the cost
-#    function.
-#
-# .. figure:: /_static/img/chatbot/grad_clip.png
-#    :align: center
-#    :width: 60%
-#    :alt: grad_clip
-#
-# Image source: Goodfellow et al. *Deep Learning*. 2016. https://www.deeplearningbook.org/
-#
-# **Sequence of Operations:**
-#
-#    1) Forward pass entire input batch through encoder.
-#    2) Initialize decoder inputs as SOS_token, and hidden state as the encoder's final hidden state.
-#    3) Forward input batch sequence through decoder one time step at a time.
-#    4) If teacher forcing: set next decoder input as the current target; else: set next decoder input as current decoder output.
-#    5) Calculate and accumulate loss.
-#    6) Perform backpropagation.
-#    7) Clip gradients.
-#    8) Update encoder and decoder model parameters.
-#
-#
-# .. Note ::
-#
-#   PyTorch’s RNN modules (``RNN``, ``LSTM``, ``GRU``) can be used like any
-#   other non-recurrent layers by simply passing them the entire input
-#   sequence (or batch of sequences). We use the ``GRU`` layer like this in
-#   the ``encoder``. The reality is that under the hood, there is an
-#   iterative process looping over each time step calculating hidden states.
-#   Alternatively, you can run these modules one time-step at a time. In
-#   this case, we manually loop over the sequences during the training
-#   process like we must do for the ``decoder`` model. As long as you
-#   maintain the correct conceptual model of these modules, implementing
-#   sequential models can be very straightforward.
-#
 #
 # if __name__ == "__main__": 
 #     main()
